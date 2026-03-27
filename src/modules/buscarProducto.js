586f79ac4029e0a7cc745fae34d79343d4f33002
SANTIAGO NICOLÁS HONG <49189314@ort>
2026-03-27 09:07:13 -0300

    Terminando ej5

--- src/modules/buscarProducto.js
+++ src/modules/buscarProducto.js
@@ -1,5 +1,14 @@
 import fs from 'fs';
 
 export default function BuscarProducto(nombre) {
+ const dato = fs.readFileSync('productos.json', 'utf-8');
+    const list = JSON.parse(dato);
 
+    econtrado = dato.find(producto => producto.Nombre === nombre);
+
+    if (econtrado) {
+        console.log(econtrado);
+    } else {
+        console.log('Producto no encontrado');
+    }
 }
